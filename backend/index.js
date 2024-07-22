3917bcc551595a6024998ce54e4f0423aae4f6b6
Yash Pounikar <79402434+yashpounikar01@users.noreply.github.com>
2024-07-22 17:28:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const path = require('path'); // Import path module
-require('dotenv').config({ path: path.resolve(__dirname, '.env') }); // Load environment variables from .env file
+const cors = require('cors'); // Import cors
+const path = require('path'); 
+require('dotenv').config({ path: path.resolve(__dirname, '.env') }); 
 
 const authRouter = require('./routes/auth');
 const lectureRouter = require('./routes/lecture');
@@ -11,12 +12,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors({
+    origin: 'https://yashattendance.netlify.app/', // Replace with your Netlify URL
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Session middleware
 app.use(session({
-    secret: process.env.SESSION_SECRET || 'your_secret_key', // Use environment variable or default
+    secret: process.env.SESSION_SECRET || 'your_secret_key', 
     resave: false,
     saveUninitialized: false
 }));
